a37ddf019250fe2c2bfe3088544f119bca7b5e43
Prince-Saha-Ayon <67217982+Prince-Saha-Ayon@users.noreply.github.com>
2024-09-06 00:23:42 +0600

    Update variables.tf

--- eks/variables.tf
+++ eks/variables.tf
@@ -32,14 +32,14 @@ variable "is-eks-cluster-enabled" {}
 variable "cluster-version" {}
 variable "endpoint-private-access" {}
 variable "endpoint-public-access" {}
-#variable "ondemand_instance_types" {
-#  default = ["t2.medium"]
-#}
+variable "ondemand_instance_types" {
+  default = ["t2.medium"]
+}
 
 variable "spot_instance_types" {}
-#variable "desired_capacity_on_demand" {}
-#variable "min_capacity_on_demand" {}
-#variable "max_capacity_on_demand" {}
+variable "desired_capacity_on_demand" {}
+variable "min_capacity_on_demand" {}
+variable "max_capacity_on_demand" {}
 variable "desired_capacity_spot" {}
 variable "min_capacity_spot" {}
 variable "max_capacity_spot" {}
